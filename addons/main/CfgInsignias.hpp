9ff96c9e4f10f653bb446eda729e2cbdb73b7905
alessandroportilho <aleportilhoti@gmail.com>
2024-10-27 01:23:13 -0300

    Added Everyone "Insignias"

--- addons/main/CfgInsignias.hpp
+++ addons/main/CfgInsignias.hpp
@@ -18,15 +18,6 @@ class CfgUnitInsignia
 		textureVehicle = "";													
 	};
 
-	class braf_insignias_19bimtz
-	{
-		displayName = "19º Batalhão De Infantaria Motorizada";								
-		author = AUTHOR;											
-		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_19_bi_mtz.paa";			
-		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
-		textureVehicle = "";													
-	};
-
 	class braf_insignias_1bac
 	{
 		displayName = "1º Batalhão de Ações de Comandos";								
@@ -139,17 +130,333 @@ class CfgUnitInsignia
 	{
 		displayName = " 10º Esquadrão de Cavalaria Mecanizado";								
 		author = AUTHOR;											
-		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_10esq_c_mec.paa";			
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_10esqd_c_mec.paa";			
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";													
 	};
 
 	class braf_insignias_12becmdbld
 	{
-		displayName = " 12º Batalhão ";								
+		displayName = " 12º Batalhão de Engenharia de Combate Blindado ";								
 		author = AUTHOR;											
-		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_10esq_c_mec.paa";			
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_12_be_cmd_bld.paa";			
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";													
 	};
+
+	class braf_insignias_13gac
+	{
+		displayName = "13° Grupo de Artilharia de Campanha";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_13_gac.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_14bimtz
+	{
+		displayName = "14º Batalhão de Infantaria Motorizado";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_14_bi_mtz.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_14ciacommec
+	{
+		displayName = "14ª Companhia de Comunicações Mecanizada";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_14cia_com_mec.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_15ciamtz
+	{
+		displayName = "15ª Companhia de Infantaria Motorizada";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_15_cia_mtz.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_17bfron
+	{
+		displayName = "17º Batalhão de Fronteira";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_17_b_fron.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_19bc
+	{
+		displayName = "19º Batalhão de Caçadores";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_19_bc.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+		class braf_insignias_19bimtz
+	{
+		displayName = "19º Batalhão De Infantaria Motorizada";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_19_bi_mtz.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+		class braf_insignias_21gac
+	{
+		displayName = "21º Grupo de Artilharia em Campanha";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_21_gac.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_22gac
+	{
+		displayName = "22° Grupo de Artilharia de Campanha Autopropulsado";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_22_gac_ap.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_23bi
+	{
+		displayName = "23º Batalhão de Infantaria";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_23_bi.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_23bdainfsl
+	{
+		displayName = "23ª Brigada de Infantaria de Selva";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_23_bda_inf_sl.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_23ciacomsl
+	{
+		displayName = "23ª Companhia de Comunicações de Selva";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_23_cia_com_sl.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_25pelpe
+	{
+		displayName = "25º Pelotão de Polícia do Exército";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_25_pel_pe.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+class braf_insignias_26bipqdt
+	{
+		displayName = "26º Batalhão de Infantaria Pára-quedista";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_26_bi_pqdt.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_27bipqdt
+	{
+		displayName = "27º Batalhão de Infantaria Pára-quedista";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_27_bi_pqdt.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_27gac
+	{
+		displayName = "27° Grupo de Artilharia de Campanha";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_27_gac.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_28blog
+	{
+		displayName = "28º Batalhão Logístico";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_28_blog.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_29bib
+	{
+		displayName = "29° Batalhão de Infantaria Blindado";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_29_bib.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_29gacap
+	{
+		displayName = "29º Grupo de Artilharia de Campanha Autopropulsado";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_29_gac_ap.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_30bimec
+	{
+		displayName = "30° Batalhão de Infantaria Mecanizado";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_30_bi_mec.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_32pelpe
+	{
+		displayName = "32° Pelotão De Polícia Do Exército";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_32_pel_pe.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_36bimec
+	{
+		displayName = "36° Batalhão de Infantaria Mecanizado";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_36_bi_mec.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_50bis
+	{
+		displayName = "50° Batalhão de Infantaria De Selva";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_50_bis.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_51bis
+	{
+		displayName = "51° Batalhão de Infantaria De Selva";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_51_bis.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_52bis
+	{
+		displayName = "52° Batalhão de Infantaria De Selva";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_52_bis.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_62bi
+	{
+		displayName = "62º Batalhão de Infantaria";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_62bi.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+	
+	class braf_insignias_71bimtz
+	{
+		displayName = " 71º Batalhão de Infantaria Motorizado";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_71_bi_mtz.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+	
+	class braf_insignias_72bimtz
+	{
+		displayName = "72º Batalhão de Infantaria Motorizado";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_72_bi_mtz.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_ad1
+	{
+		displayName = "Artilharia Divisionária da 1ª Divisão de Exército";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_ad1.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+	
+	class braf_insignias_ad3
+	{
+		displayName = "Artilharia Divisionária da 3ª Divisão de Exército";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_ad3.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_bdompsa
+	{
+		displayName = "Batalhão de Dobragem, Manutenção de Paraquedas e Suprimentos pelo Ar";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_bdompsa.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_brasilext
+	{
+		displayName = "Insignia Brasileira De Operações Exteriores";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_brasil_ext.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_ciadqbrnwhite
+	{
+		displayName = "Batalhão de Defesa Química Biológica Radiológica e Nuclear";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_cia_dqbrn_white.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_ciadqbrnblack
+	{
+		displayName = "Batalhão de Defesa Química Biológica Radiológica e Nuclear (Preto)";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_cia_dqbrn_black.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
+	class braf_insignias_cma
+	{
+		displayName = "Comando Militar da Amazônia";								
+		author = AUTHOR;											
+		texture = "braf\braf_insignias\addons\braf_insignias_main\data\eb_cma.paa";			
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";													
+	};
+
 };
